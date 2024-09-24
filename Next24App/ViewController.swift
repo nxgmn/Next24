d5db3ad93aab73eb1e0f894943303a1ba1dd1f15
caseyechin <caseyc611@gmail.com>
2024-09-23 22:17:09 -0400

    Initial Commit

---
+++ Next24App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Next24App
+//
+//  Created by Casey Chin on 9/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
